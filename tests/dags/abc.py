d3e572d73761b2a036eefa43d00ca9e57d910f87
Heino H. Gehlsen <heino@gehlsen.dk>
2024-01-25 19:23:16 +0100

    Update abc.py

--- tests/dags/abc.py
+++ tests/dags/abc.py
@@ -5,7 +5,7 @@ from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="my_dag_name",
-    start_date=datetime.datetime(2021, 1, 1),
+    start_date=datetime.datetime(2024, 1, 1),
     schedule="@daily",
 ):
     EmptyOperator(task_id="task")
